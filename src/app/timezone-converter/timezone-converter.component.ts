b273ac9ce20d69bbedbb861f662c402ae094de02
Leonid Abasov <leonid.abasov@gmail.com>
2025-01-25 17:41:10 -0600

    fix: use itl time zones

--- src/app/timezone-converter/timezone-converter.component.ts
+++ src/app/timezone-converter/timezone-converter.component.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-timezone-converter',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './timezone-converter.component.html',
   styleUrls: ['./timezone-converter.component.css'],
 })
 export class TimezoneConverterComponent {
-  timeZones: string[] = DateTime.now().setZone('utc').availableZones;
+  timeZones: string[] = Intl.supportedValuesOf('timeZone');;
   userTimeZone: string = Intl.DateTimeFormat().resolvedOptions().timeZone;
   targetTimeZone: string = 'UTC';
   inputTime: string = '';
